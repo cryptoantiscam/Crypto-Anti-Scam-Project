b4e2ebd58ae730969c03fbb5c7f2346f2b0c5d9d
cryptosafedev <cryptosafedev@gmail.com>
2018-04-20 00:30:03 +0100

    reverse commit.

--- js/popup.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function () {
-	var currentscams = localStorage.getItem('scams');
-  	var currentspams = localStorage.getItem('spams');
-  	var totalcount = parseInt(currentscams)+parseInt(currentspams);
-  	$('#totalcount').text(totalcount);
-  	$('#scamscount').text(currentscams);
-  	$('#spamscount').text(currentspams);
-});
- 
